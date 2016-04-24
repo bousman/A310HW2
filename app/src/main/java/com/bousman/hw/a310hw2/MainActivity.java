b3c3c953d8512154b22779d81569473a386c5550
bousman <bousman@gmail.com>
2016-04-23 19:35:26 -0500

    added Facebook sdk

--- app/src/main/java/com/bousman/hw/a310hw2/MainActivity.java
+++ app/src/main/java/com/bousman/hw/a310hw2/MainActivity.java
@@ -1,7 +1,11 @@
 package com.bousman.hw.a310hw2;
 
+
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
 import com.facebook.FacebookSdk;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,4 +15,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onClickButton(View view) {
+
+            Intent explicitIntent = new Intent(MainActivity.this, FacebookActivity.class);
+            startActivity(explicitIntent);
+    }
+
 }
